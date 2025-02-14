329c3fa66ed2422b94afcc35cae4a4d0f939adcd
Cybk <Cybk2000@hotmail.com>
2025-02-13 21:59:30 -0600

    Initial commit for RPG practice

---
+++ 02_RPG_Spyder/02_RPG_Spyder/Classes/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 21:52:04 2025
+
+@author: cybk2
+"""
+
---
+++ 02_RPG_Spyder/02_RPG_Spyder/Classes/game.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 21:54:22 2025
+
+@author: cybk2
+"""
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
